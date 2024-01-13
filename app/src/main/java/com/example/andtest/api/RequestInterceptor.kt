376e01d27a9a786eb69497c9d1004a19d0a1c596
Pawel <gussy1258@gmail.com>
2024-01-13 18:22:12 +0100

    unused

--- app/src/main/java/com/example/andtest/api/RequestInterceptor.kt
+++ app/src/main/java/com/example/andtest/api/RequestInterceptor.kt
@@ -3,7 +3,7 @@ package com.example.andtest.api
 import okhttp3.Interceptor
 import okhttp3.Response
 
-object RequestInterceptor : Interceptor {
+class RequestInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
         println("Outgoing request to ${request.url()}")
