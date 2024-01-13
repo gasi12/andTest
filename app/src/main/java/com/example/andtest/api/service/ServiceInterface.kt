d2cc4b85378a55b594597c1414b2b0c07c3cdab5
Pawel <gussy1258@gmail.com>
2024-01-13 18:18:58 +0100

    added new methods

--- app/src/main/java/com/example/andtest/api/service/ServiceInterface.kt
+++ app/src/main/java/com/example/andtest/api/service/ServiceInterface.kt
@@ -2,7 +2,9 @@ package com.example.andtest.api.service
 
 import com.example.andtest.api.dto.Customer
 import com.example.andtest.api.dto.CustomerAndDevicesAndServiceRequestsDto
+import com.example.andtest.api.dto.CustomerAndDevicesAndServiceResponseDto
 import com.example.andtest.api.dto.CustomerWithDevicesListDtoResponse
+import com.example.andtest.api.dto.Device
 import com.example.andtest.api.dto.LoginRequest
 import com.example.andtest.api.dto.RefreshTokenRequest
 import com.example.andtest.api.dto.LoginResponse
@@ -10,6 +12,8 @@ import com.example.andtest.api.dto.ServiceRequest
 import com.example.andtest.api.dto.ServiceRequestWithDetailsDto
 import com.example.andtest.api.dto.ServiceRequestWithUserNameDtoResponse
 import com.example.andtest.api.dto.StatusHistoryDtoRequest
+import retrofit2.Call
+import retrofit2.http.Path
 
 interface ServiceInterface {
     fun loginCall(body: LoginRequest, callback: (LoginResponse?, Boolean) -> Unit)
@@ -26,9 +30,11 @@ interface ServiceInterface {
 
     fun getCustomerWithDevicesByPhoneNumber(phoneNumber: Long, callback: (CustomerWithDevicesListDtoResponse?, Boolean) -> Unit)
 
-    fun addCustomerWithDeviceAndService(body: CustomerAndDevicesAndServiceRequestsDto, callback: (CustomerAndDevicesAndServiceRequestsDto?,Boolean)->Unit)
+    fun addCustomerWithDeviceAndService(body: CustomerAndDevicesAndServiceRequestsDto, callback: (CustomerAndDevicesAndServiceResponseDto?, Boolean)->Unit)
 
     fun editService(id : Long, body: ServiceRequest, callback: (Boolean) -> Unit)
     fun getCustomerList(page: Int?,pageSize: Int?, callback: (List<Customer>)-> Unit)
+
+    fun getDeviceList(page: Int?,pageSize: Int?, callback: (List<Device>)-> Unit)
 
 }
