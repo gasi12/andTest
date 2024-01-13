5c48eb35df02afef27656538e1ef912e5b56e95e
Pawel <gussy1258@gmail.com>
2024-01-13 18:22:59 +0100

    values not private anymore

--- app/src/main/java/com/example/andtest/api/dto/CustomerAndDevicesAndServiceRequestsDto.kt
+++ app/src/main/java/com/example/andtest/api/dto/CustomerAndDevicesAndServiceRequestsDto.kt
@@ -1,10 +1,9 @@
 package com.example.andtest.api.dto
 
 data class CustomerAndDevicesAndServiceRequestsDto(
-        private val customer: Customer,
-    private val device: Device,
-    private val serviceRequest: ServiceRequest
+         val customer: Customer,
+     val device: Device,
+     val serviceRequest: ServiceRequest
 )
-
 
 
