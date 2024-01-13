3e7e05eac8229fe13ec4600320359679dde9a6d3
Pawel <gussy1258@gmail.com>
2024-01-13 18:22:29 +0100

    added visible names

--- app/src/main/java/com/example/andtest/api/dto/DeviceType.kt
+++ app/src/main/java/com/example/andtest/api/dto/DeviceType.kt
@@ -2,12 +2,12 @@ package com.example.andtest.api.dto
 
 
 enum class DeviceType(val visibleName: String){
-    LAPTOP("laptop"),
-    DESKTOP("desktop"),
-    SMARTPHONE("smartphone"),
-    TV("tv"),
-    PRINTER("printer or scanner"),
-    MONITOR("monitor"),
-    GAME_CONSOLE("game console"),
-    OTHER("other")
+    LAPTOP("Laptop"),
+    DESKTOP("Desktop"),
+    SMARTPHONE("Smartphone"),
+    TV("Tv"),
+    PRINTER("Printer or scanner"),
+    MONITOR("Monitor"),
+    GAME_CONSOLE("Console"),
+    OTHER("")
 }
